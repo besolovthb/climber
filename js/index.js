ba9991d28e3e7e90343485eb802f48d1816fa58b
besolovthb <abdullahbesolov@gmail.com>
2023-05-20 18:39:33 +0200

    first js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
+const klimmer = document.querySelector('#climber');
+const strepen = document.querySelector('#')
+
+
+
+
+
+
 console.log('Hello from index.js')
 
 addEventListener('keyup', (e) => {
   if (e.key == "d" ) {
     console.log('You pressed the D key!')
+  }
+  if (e.key == 'd') {
+    klimmer.style.position === right;
+  }
+})
+
+
+
+addEventListener('keyup', (e) => {
+  if (e.key == "a" ) {
+    console.log('You pressed the A key!')
+  }
+  if (e.key == 'a') {
+    klimmer.style.position === left;
   }
 })
